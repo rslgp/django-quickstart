98e4e4e53b8bf35216d2c050a1a01ee28dbcb014
rslgp <rslgp@cin.ufpe.br>
2023-10-31 17:17:33 -0300

    Update urls.py

--- AuthExample/SistemaWeb/SistemaWeb/urls.py
+++ AuthExample/SistemaWeb/SistemaWeb/urls.py
@@ -19,8 +19,6 @@ from django.urls import path, include
 
 from django.views.generic.base import TemplateView
 
-from auth.views import signup_view, logout_view
-
 urlpatterns = [
     path('admin/', admin.site.urls),
 
@@ -29,8 +27,6 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 
     #--- USER AUTH
-    path('accounts/', include('django.contrib.auth.urls')),    
-    path('signup/', signup_view, name='signup'),
-    path('logout/', logout_view, name='logout'),
+    path('', include('auth.urls')),
     
 ]
